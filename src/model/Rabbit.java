9a7856836e3313c09d5cf1bfc78e92cef6ba6953
noahmank <noah.rmank@gmail.com>
2019-10-19 16:15:52 -0400

    Remove MoveablePiece class and tweak class relationships

--- src/model/Rabbit.java
+++ src/model/Rabbit.java
@@ -1,25 +1,29 @@
 package model;
 
-public class Rabbit extends MoveablePiece{
-	
+public class Rabbit implements Piece{
+	String color;
 	/**
 	 * The Rabbit constructor constructs a new Rabbit constructor
 	 * @param color identifies the color of the Rabbit
 	 * @param row the X coordinate of the Rabbit
 	 * @param column the Y coordinate of the Rabbit
 	 */
-	public Rabbit(String color, int row, int column) {
-		super(color, row, column);
+	public Rabbit(String color) {
+		this.color = color;
 	}
 	
 	/**
 	 * A text representation of the Rabbit
 	 */
+	private String getColor() {
+		return this.color;
+	}
+	
 	public String toString() {
-		if(this.getName().toUpperCase() == "BROW") {
+		if(this.getColor().toUpperCase() == "BROWN") {
 			return "RR";
 		}
-		else if(this.getName().toUpperCase() == "BLACK") {
+		else if(this.getColor().toUpperCase() == "BLACK") {
 			return "BR";
 		}
 		else return "WR";
