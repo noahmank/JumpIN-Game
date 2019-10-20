a313ff0d692308b6805ee2e11a873faa41b1fedc
Liya Abebe <43866253+liyaabebe@users.noreply.github.com>
2019-10-20 17:54:53 -0400

    Direction values for 4th quadrant

--- src/model/Direction.java
+++ src/model/Direction.java
@@ -1,10 +1,10 @@
 package model;
 
 public enum Direction {
-	NORTH(0, 1),
-	EAST(1, 0), 
-	SOUTH(0, -1), 
-	WEST(-1, 0);
+	NORTH(0,-1);
+	EAST(-1,0);
+	SOUTH(0,1);
+	WEST(1,0);
 	
 	private int x;
 	private int y;
