f9a3bcc202417db7e037602d832988ff49ac6587
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-10-20 21:27:51 -0400

    Adding javadoc to Mushroom

--- src/model/Mushroom.java
+++ src/model/Mushroom.java
@@ -1,10 +1,19 @@
 package model;
-
+/**
+* Mushroom class that initializes a mushroom
+*/
 public class Mushroom implements Piece{
+	/**
+	* Default constructor
+	*/
 	public Mushroom() {
 		
 	}
 	
+	/**
+	* Method that converts the mushroom to a string
+	* @return string representaiton of mushroom
+	*/
 	@Override
 	public String toString() {
 		return "MS";
