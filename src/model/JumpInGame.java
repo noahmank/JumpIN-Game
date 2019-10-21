d8b749c3e7bc15951f2e39c3b4947d348f2c8f7b
noahmank <noah.rmank@gmail.com>
2019-10-21 16:59:52 -0400

    Clean up redundant code and edit visibility of methods

--- src/model/JumpInGame.java
+++ src/model/JumpInGame.java
@@ -20,15 +20,6 @@ public class JumpInGame {
 		gameStatus = GameStatus.READY_TO_PLAY;
 	}
 	
-	
-	/**
-	 * the getGameStatus method retrieves the status of the game
-	 * @return READY_TO_PLAY, IN_PROGRESS or FINISHED
-	 */
-	public GameStatus getGameStatus() {
-		return gameStatus;
-	}
-
 	/**
 	 * the start method verifies if the game is ready to play
 	 */
@@ -39,9 +30,10 @@ public class JumpInGame {
 		}
 		else {
 			System.out.println("Your game is not ready to play");
+			return;
 		}
 		
-		System.out.println("			Welcome to The JumpIn Game.");
+		System.out.println("\t\t\tWelcome to The JumpIn Game.");
 		System.out.println("GAME RULES\n 1) Select a challenge (Numbers between 1 and 1).\n "
 				+ "2) The Objective of the game is to move the rabbits and foxes around the gameboard until all of the rabbits are safe in brown holes.\n"
 				+ " 3) You have found a solution when all of the rabbits are inside brown holes!\n");
@@ -56,39 +48,31 @@ public class JumpInGame {
 		this.challenge(input);
 		
 		while(!this.gameBoard.isFinished()) {
+			displayBoard();
 			System.out.println("Select a moveable piece from the list above that you would like to move (Use Legend title e.g F1 or WR): ");
 			
-			String piece = scanner.next();
+			String piece = scanner.next().toUpperCase();
 			
 			if(piece.equals("WR") | piece.equals("GR") | piece.equals("BR")) {
-			
-				System.out.println("");
-				System.out.println("You can move the Rabbit EAST, WEST, NORTH or SOUTH.\n Enter the direction in which you would like to move the rabbit: ");
+				System.out.println("\nYou can move the Rabbit EAST, WEST, NORTH or SOUTH.\n Enter the direction in which you would like to move the rabbit: ");
 				
 				String directionInput = scanner.next();
-				
-				Direction Rdirection = (directionInput.toUpperCase().equals("EAST"))? Direction.EAST : (directionInput.toUpperCase().equals("WEST"))? Direction.WEST :(directionInput.toUpperCase().equals("NORTH"))? Direction.NORTH : (directionInput.toUpperCase().equals("SOUTH"))? Direction.SOUTH : null;
+				Direction Rdirection = Direction.valueOf(directionInput.toUpperCase());
 				
 				try{
 					this.jumpRabbit(piece, Rdirection);
 				}
 				catch(Exception e) {
 					System.out.println("Direction invalid, choose another.\n");
-					System.out.println(gameBoard.toString() +"\n\n" + gameBoard.getLegend());
-
 				}
-
 			}
 			
 			if(piece.equals("F1") | piece.equals("F2")) {
-				
-				System.out.println("");
-				System.out.println("You can slide the fox parallel to its current position.\n"
+				System.out.println("\nYou can slide the fox parallel to its current position.\n"
 						+ "Enter the direction in which you would like to slide the fox: ");
 				
 				String directionInput = scanner.next();
-				
-				Direction Fdirection = (directionInput.toUpperCase().equals("EAST"))? Direction.EAST : (directionInput.toUpperCase().equals("WEST"))? Direction.WEST :(directionInput.toUpperCase().equals("NORTH"))? Direction.NORTH : (directionInput.toUpperCase().equals("SOUTH"))? Direction.SOUTH : null;
+				Direction Fdirection = Direction.valueOf(directionInput.toUpperCase());
 				
 				try{
 					this.slideFox(piece, Fdirection);
@@ -108,10 +92,9 @@ public class JumpInGame {
 	 * @param name is the identity of the fox e.g F1
 	 * @param direction is the desired direction that the fox is being moved to
 	 */
-	public void slideFox(String name, Direction direction) {
+	private void slideFox(String name, Direction direction) {
 		gameBoard.moveFoxPiece(name, direction);
 		System.out.println("");
-		displayBoard();
 	}
 	
 	/**
@@ -119,16 +102,15 @@ public class JumpInGame {
 	 * @param name is the identity of the rabbit e.g Black
 	 * @param direction is the desired direction that the rabbit is being moved to
 	 */
-	public void jumpRabbit(String name, Direction direction) {
+	private void jumpRabbit(String name, Direction direction) {
 		gameBoard.moveRabbitPiece(name, direction);
 		System.out.println("");
-		displayBoard();
 	}
 	
 	/**
 	 * the displayBoard method reflects the appearance of the board
 	 */
-	public void displayBoard() {
+	private void displayBoard() {
 		System.out.println(gameBoard.toString());
 		System.out.println(gameBoard.getLegend());
 	}
@@ -147,7 +129,7 @@ public class JumpInGame {
 	 * The challenge method constructs the board for the specified challenge
 	 * @param challenge is the number that identifies the challenge
 	 */
-	public void challenge(int challenge) {
+	private void challenge(int challenge) {
 		
 		if(challenge == 1) {
 			this.addPieceToBoard(new Rabbit("Grey"), 3, 0);
@@ -157,7 +139,6 @@ public class JumpInGame {
 			this.addPieceToBoard(new Fox(Direction.EAST, 2), 3, 3);
 			this.addPieceToBoard(new Mushroom(), 3, 1);
 			this.addPieceToBoard(new Mushroom(), 2, 4);
-			displayBoard();
 		}
 	}
 	
@@ -166,7 +147,6 @@ public class JumpInGame {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
 		JumpInGame game = new JumpInGame();
 		game.start();
 	}
