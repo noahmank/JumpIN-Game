4b778251237c8ecc9a4ea04ec81b848f5127e171
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-02 16:50:42 -0400

    Update All Tests test suite

--- src/model/AllTests.java
+++ src/model/AllTests.java
@@ -1,5 +1,8 @@
 package model;
-
+/**
+* Test suite for all the tests within this package
+* @author Adela Tullio
+*/
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
