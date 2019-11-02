f55db1cbf1c09e46ad782105f10053f696899c62
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-02 16:07:46 -0400

    Update GameBoardTest

--- src/model/GameBoardTest.java
+++ src/model/GameBoardTest.java
@@ -34,43 +34,55 @@ public class GameBoardTest {
 	
 	@Test
 	public void testResetBoard() {
-		//assertEquals()
 		gameboard.resetBoard();
+		assertEquals("Expected 'BH'", "BH", gameboard.getHoleContent(4, 4).toString());
+		assertEquals("Expected '  '", "  ", gameboard.getHoleContent(1, 2).toString());
+		assertEquals("Expected 'RH'", "RH", gameboard.getHoleContent(4, 2).toString());
 	}
 	
 	@Test
 	public void testAddingMushroomPiece() {
-		gameboard.addPiece(mushroom, 2, 2);
-		//assertEquals()
+		gameboard.addPiece(mushroom, 2, 1);
+		assertEquals("Expecting 'MS' in column 2, row 2", "MS", gameboard.getHoleContent(2, 1).toString());
 	}
 	
 	@Test
 	public void testAddingFoxPiece() {
-		gameboard.addPiece(fox, 3, 5);
+		gameboard.addPiece(fox, 3, 2);
+		assertEquals("Expecting 'F1'", "F1", gameboard.getHoleContent(3, 2).toString());
 	}
 	
 	@Test
 	public void testAddingRabbitPiece() {
-		gameboard.addPiece(rabbit, 3, 1);
-	}
-	
-	@Test
-	public void testToString() {
-		gameboard.toString();
+		gameboard.addPiece(rabbit, 0, 2);
+		assertEquals("Expecting 'GR'", "GR", gameboard.getHoleContent(0,  2).toString());
 	}
 	
 	@Test
 	public void testMoveFoxPiece() {
+		gameboard.addPiece(fox, 3, 2);
 		gameboard.moveFoxPiece("F1", direction.NORTH);
+		assertEquals("Expecting 'F1'", "F1", gameboard.getHoleContent(3, 0).toString());
+		
 	}
 	
 	@Test
 	public void testMoveRabbitPiece() {
+		gameboard.addPiece(mushroom, 1, 2);
+		gameboard.addPiece(rabbit, 0, 2);
 		gameboard.moveRabbitPiece("GR", direction.EAST);
+		assertEquals("Expecting 'GR'.", "GR", gameboard.getHoleContent(2, 2).toString());
 	}
 	
 	@Test
 	public void testIsFinished() {
+		Rabbit rabbit2 = new Rabbit("Brown");
+		Rabbit rabbit3 = new Rabbit("White");
 		
+		gameboard.addPiece(rabbit, 0, 0);
+		gameboard.addPiece(rabbit2, 4, 0);
+		gameboard.addPiece(rabbit3, 0, 4);
+		
+		assertEquals("Expecting a value true.", true, gameboard.isFinished());
 	}
 }	
