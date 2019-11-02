b04615f0d77749216f45475d9c235a4649290bf2
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-02 14:48:39 -0400

    Adding to GameBoardTest.java

--- src/model/GameBoardTest.java
+++ src/model/GameBoardTest.java
@@ -1,3 +1,4 @@
+package model;
 
 import static org.junit.Assert.*;
 
@@ -8,51 +9,64 @@ import org.junit.Test;
 public class GameBoardTest {
 	
 	private GameBoard gameboard;
+	private Direction direction;
+	private Mushroom mushroom;
+	private Fox fox;
+	private Rabbit rabbit;
 	
 	@Before
 	public void setUp() throws Exception {
 		gameboard = new GameBoard();
+		mushroom = new Mushroom();
+		fox = new Fox(direction.NORTH, 1);
+		rabbit = new Rabbit("Grey");
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		gameboard = null;
+		rabbit = null;
+		direction = null;
+		mushroom = null;
+		fox = null;
 	}
 
 	
 	@Test
 	public void testResetBoard() {
-		
+		//assertEquals()
+		gameboard.resetBoard();
 	}
 	
 	@Test
 	public void testAddingMushroomPiece() {
-		
+		gameboard.addPiece(mushroom, 2, 2);
+		//assertEquals()
 	}
 	
 	@Test
 	public void testAddingFoxPiece() {
-		
+		gameboard.addPiece(fox, 3, 5);
 	}
 	
 	@Test
 	public void testAddingRabbitPiece() {
-		
+		gameboard.addPiece(rabbit, 3, 1);
 	}
 	
 	@Test
 	public void testToString() {
-		
+		gameboard.toString();
 	}
 	
 	@Test
 	public void testMoveFoxPiece() {
-		
+		gameboard.moveFoxPiece("F1", direction.NORTH);
 	}
 	
 	@Test
 	public void testMoveRabbitPiece() {
-		
+		gameboard.moveRabbitPiece("GR", direction.EAST);
 	}
 	
 	@Test
