1da9c41fc9d69e83277b2e8dfb85860ff928c824
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-02 10:44:54 -0400

    Editing test suite

--- src/model/AllTests.java
+++ src/model/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BrownHoleTest.class, DirectionTest.class, FoxTest.class, GameBoardTest.class, GameStatusTest.class,
+@SuiteClasses({ BrownHoleTest.class, FoxTest.class, GameBoardTest.class, GameStatusTest.class,
 		HoleTest.class, JumpInGameTest.class, MushroomTest.class, PieceTest.class, RabbitTest.class,
 		RaisedHoleTest.class })
 public class AllTests {
