26bf9f5cac8230036559447cefb5246a365aa5ae
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-02 17:16:52 -0400

    Update MushroomTest

--- src/model/MushroomTest.java
+++ src/model/MushroomTest.java
@@ -1,21 +1,34 @@
 package model;
-
+/**
+* Tests for the Mushroom class implementation
+* @author Adela Tullio
+*/
 import static org.junit.Assert.*;
 
 public class MushroomTest {
 
 	private Mushroom mushroom;
 	
+	/**
+	* Sets up the variables
+	* @throws Exception
+	*/
 	@Before
 	public void setUp() throws Exception {
 		mushroom = new Mushroom();
 	}
-
+	
+	/**
+	* Tears down the variables
+	* @throws Exception
+	*/
 	@After
 	public void tearDown() throws Exception {
 		mushroom = null;
 	}
-
+	/**
+	* Tests the toString method in the Mushroom class
+	*/
 	@Test
 	public void testToString() {
 		assertEquals("Should be 'MS'.", "MS", mushroom.toString());
