dbc49f5c65a2e2f07347b315dc52b39a77996493
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-02 12:30:04 -0400

    Create GameBoardTest.java

---
+++ src/model/GameBoardTest.java
@@ -0,0 +1,62 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameBoardTest {
+	
+	private GameBoard gameboard;
+	
+	@Before
+	public void setUp() throws Exception {
+		gameboard = new GameBoard();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		gameboard = null;
+	}
+
+	
+	@Test
+	public void testResetBoard() {
+		
+	}
+	
+	@Test
+	public void testAddingMushroomPiece() {
+		
+	}
+	
+	@Test
+	public void testAddingFoxPiece() {
+		
+	}
+	
+	@Test
+	public void testAddingRabbitPiece() {
+		
+	}
+	
+	@Test
+	public void testToString() {
+		
+	}
+	
+	@Test
+	public void testMoveFoxPiece() {
+		
+	}
+	
+	@Test
+	public void testMoveRabbitPiece() {
+		
+	}
+	
+	@Test
+	public void testIsFinished() {
+		
+	}
+}	
