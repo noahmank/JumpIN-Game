f10c6dd93dc15c961a6bbb63a6b937317e42faaa
Noah <Noah@172.17.51.151>
2019-11-02 14:08:10 -0400

    Add main to create window

--- src/view/DirectionButtonGrid.java
+++ src/view/DirectionButtonGrid.java
@@ -5,11 +5,17 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class DirectionButtonGrid extends JFrame {
-	DirectionButtonGrid() {
+	public DirectionButtonGrid() {
 		setLayout(new GridLayout(1, 4));
 		add(new JButton("WEST"));
 		add(new JButton("NORTH"));
 		add(new JButton("EAST"));
 		add(new JButton("SOUTH"));
+	}
+
+
+	public static void main(String[] args) {
+		DirectionButtonGrid b = new DirectionButtonGrid();
+		b.setVisible(true);
 	}
 }
