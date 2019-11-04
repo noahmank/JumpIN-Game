bfd6c9658355022b890fef10bd5480def98704ef
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-04 11:53:31 -0500

    Added JavaDoc

--- src/view/DirectionButton.java
+++ src/view/DirectionButton.java
@@ -1,5 +1,7 @@
 package view;
-
+/**
+* DirectionButton class to set up the buttons for directions
+*/
 import javax.swing.JButton;
 
 import model.*;
@@ -7,10 +9,16 @@ import model.*;
 public class DirectionButton extends JButton {
 	private Direction direction;
 	
+	/**
+	* @param d is the direction you wish the button to be
+	*/
 	public DirectionButton(Direction d) {
 		this.direction = d;
 	}
 	
+	/**
+	* @return the direction
+	*/
 	public Direction getDirection() {
 		return this.direction;
 	}
