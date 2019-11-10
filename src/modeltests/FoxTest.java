88dc854250f429de37973b62e40c7ae21f138326
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-09 19:42:05 -0500

    Added tests for equals method

--- src/modeltests/FoxTest.java
+++ src/modeltests/FoxTest.java
@@ -55,4 +55,35 @@ public class FoxTest {
 	public void testToString() {
 		assertEquals("Should be 'Fox 1'.", "Fox 1", fox.toString());
 	}
+	
+	
+	/**
+	 * Test equals method with different foxes
+	 */
+	@Test
+	public void testEqualsMethodWithDifferentFoxes() {
+		Fox fox2 = new Fox(Direction.SOUTH, 2);
+		assertEquals("Should be 'false'", false, fox.equals(fox2));
+	}
+	
+	/**
+	 * Test equals method with foxes 
+	 * containing same values
+	 */
+	@Test
+	public void testEqualsMethodWithSameFoxes() {
+		Fox fox2 = new Fox(Direction.NORTH, 1);
+		assertEquals("Should be 'true'", true, fox.equals(fox2));
+	}
+	
+	/**
+	 * Test equals method with
+	 * objects of different classes
+	 */
+	@Test
+	public void testEqualsMethodWithDifferentObjects() {
+		Mushroom mushroom = new Mushroom();
+		assertEquals("Should be 'false'", false, fox.equals(mushroom));
+	}
+
 }
