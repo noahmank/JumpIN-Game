80c9979557312d9876f51138d35a1d8f94ed8d8d
Noah <Noah@172.17.39.120>
2019-11-14 09:40:58 -0500

    Add BoardTree and BoardTreeNode classes with unimplemented methods

---
+++ src/model/BoardTreeNode.java
@@ -0,0 +1,51 @@
+package model;
+
+import java.util.Enumeration;
+
+import javax.swing.tree.*;
+
+public class BoardTreeNode implements TreeNode {
+
+	@Override
+	public Enumeration children() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean getAllowsChildren() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public TreeNode getChildAt(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getChildCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getIndex(TreeNode arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public TreeNode getParent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isLeaf() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
