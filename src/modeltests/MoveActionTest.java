4ff746f45990d672dacbc0ddcbc98b8ab48c58e3
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-14 17:44:40 -0500

    Create MoveActionTest.java

---
+++ src/modeltests/MoveActionTest.java
@@ -0,0 +1,67 @@
+package modeltests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import model.MoveAction;
+import model.Rabbit;
+import model.RabbitColor;
+import model.Direction;
+
+/**
+ * Test class for the MoveAction class implementation
+ * @author Adela Tullio
+ *
+ */
+public class MoveActionTest {
+	private MoveAction moveAction;
+	private Rabbit rabbit;
+	
+	/**
+	* Sets up the variables
+	* @throws Exception
+	*/
+	@Before
+	public void setUp() throws Exception {
+		rabbit = new Rabbit(RabbitColor.WHITE);
+		moveAction = new MoveAction(rabbit, Direction.SOUTH);
+	}
+	
+	/**
+	* Tear down for the variables
+	* @throws Exception
+	*/
+	@After
+	public void tearDown() throws Exception {
+		rabbit = null;
+		moveAction = null;
+	}
+	
+	/**
+	 * tests the getDirection method
+	 */
+	@Test
+	public void testGetDirection() {
+		assertEquals("Expected output to be SOUTH", Direction.SOUTH, moveAction.getDirection());
+	}
+	
+	/**
+	 * tests the getOppositeDirection method
+	 */
+	@Test
+	public void testGetOppositeDirection() {
+		assertEquals("Expected output to be NORTH", Direction.NORTH, moveAction.getOppositeDirection());
+	}
+	
+	/**
+	 * tests the getPiece method
+	 */
+	@Test
+	public void testGetPiece() {
+		assertEquals("Expected output to be 'White Rabbit'", "White Rabbit", moveAction.getPiece().toString());
+	}
+
+}
