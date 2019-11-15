dd02beab935fbc6884019884acff36f5da87c8ca
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-14 21:53:36 -0500

    Update AllTests test suite

--- src/modeltests/AllTests.java
+++ src/modeltests/AllTests.java
@@ -1,16 +1,16 @@
 package modeltests;
-/**
-* Test suite for all the tests within this package
-* @author Adela Tullio
-*/
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+/**
+* Test suite for all the tests 
+* @author Adela Tullio
+*/
 @RunWith(Suite.class)
-@SuiteClasses({ BrownHoleTest.class, FoxTest.class, GameBoardTest.class,
-		HoleTest.class, MushroomTest.class, RabbitTest.class,
-		RaisedHoleTest.class })
+@SuiteClasses({ BoardSolverTest.class, BoardTreeNodeTest.class, BoardTreeTest.class, BrownHoleTest.class,
+		DirectionTest.class, FoxTest.class, GameBoardTest.class, HoleTest.class, MoveActionTest.class,
+		MushroomTest.class, RabbitColorTest.class, RabbitTest.class, RaisedHoleTest.class })
 public class AllTests {
 
 }
