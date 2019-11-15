64f10521dce6b9b301358bdc287dc5651042e865
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-14 22:05:18 -0500

    Create empty BoardSolverTest.java

---
+++ src/modeltests/BoardSolverTest.java
@@ -0,0 +1,46 @@
+package modeltests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import model.*;
+/**
+ * Tests the implementation of the BoardSolver
+ * @author Adela Tullio
+ *
+ */
+public class BoardSolverTest {
+	/**
+	 * sets up variables for the tests
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	/**
+	 * tears down the variables afterwards
+	 * @throws Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * tests the getSolvedBoard method
+	 */
+	@Test
+	public void testGetSolvedBoard() {
+	}
+	
+	/**
+	 * tests the populateBoardTree method 
+	 */
+	@Test
+	public void testPopulateBoardTree() {
+		
+	}
+
+}
