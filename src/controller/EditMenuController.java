2830133c3c9a0ecfae4c523a38a92c662115158c
noahmank <noah.rmank@gmail.com>
2019-11-17 13:20:41 -0500

    Remove unused import

--- src/controller/EditMenuController.java
+++ src/controller/EditMenuController.java
@@ -2,11 +2,9 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JMenuItem;
-
 import model.*;
-import view.*;
+
 
 public class EditMenuController implements ActionListener {
 	private JumpInGame game;
