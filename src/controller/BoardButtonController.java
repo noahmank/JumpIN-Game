4865edbccddcbfb3116ab10954590e727a96ebd4
noahmank <noah.rmank@gmail.com>
2019-11-17 02:38:06 -0500

    Implement BoardButtonController

--- src/controller/BoardButtonController.java
+++ src/controller/BoardButtonController.java
@@ -2,12 +2,35 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import model.MoveablePiece;
+import view.BoardButton;
+import model.*;
+import view.*;
 
 public class BoardButtonController implements ActionListener {
-
+	JumpInGame game;
+	JumpInView view;
+	MoveablePiece piece;
+	Direction direction;
+	
+	public BoardButtonController(JumpInGame game, JumpInView view) {
+		this.game = game;
+		this.view = view;
+	}
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		BoardButton b = (BoardButton) e.getSource();
+		int row = b.getRow();
+		int column = b.getColumn();
+		if (game.getBoard().getHole(column, row).isOccupied() && (game.getBoard().getHole(column, row).getPiece() instanceof MoveablePiece)) {
+			piece = (MoveablePiece) game.getBoard().getHole(column, row).getPiece();
+		}
+		if ((direction != null) && (piece != null)) {
+			game.movePiece(piece, direction);
+			direction = null;
+		}
 		
 	}
 
