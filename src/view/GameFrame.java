7308bea97d396d864186404a660ccec35777c7fd
noahmank <noah.rmank@gmail.com>
2019-11-17 15:37:30 -0500

    Separate JumpInView into GameFrame

---
+++ src/view/GameFrame.java
@@ -0,0 +1,31 @@
+package view;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import model.JumpInGame;
+
+public class GameFrame extends JFrame {
+	private ArrowPanel arrowPanel;
+	private GridPanel gridPanel;
+	private ConsoleOutputPanel consoleOutputPanel;
+	private GameMenuBar menuBar;
+	
+	public GameFrame(JumpInGame game, JumpInView view) {
+		super("Jump-In Game");
+		
+    	this.gridPanel = new GridPanel(game);
+        this.arrowPanel = new ArrowPanel(game);
+        this.consoleOutputPanel = new ConsoleOutputPanel(game);
+        this.menuBar = new GameMenuBar(game, view);
+        
+        this.setSize(700, 700);
+        this.setLocationRelativeTo(null);
+        this.setJMenuBar(menuBar);
+        this.add(arrowPanel, BorderLayout.SOUTH);	
+    	this.add(gridPanel, BorderLayout.CENTER);
+    	this.add(consoleOutputPanel, BorderLayout.NORTH);    	
+    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+	}
+	
+}
--- src/view/JumpInView.java
+++ src/view/JumpInView.java
@@ -3,18 +3,13 @@ package view;
 * The view implementation for the game
 *
 */
-import java.awt.*;
 import javax.swing.*;
-import controller.JumpInController;
 import model.*;
 
 public class JumpInView extends JFrame {
-	private JumpInGame game;
-	private JPanel gridPanel;
-	private JFrame frame;
-	private JFrame preFrame;
-	private JPanel arrowPanel;
-	private JPanel textPanel;
+	private GameFrame gameFrame;
+	private InstructionsFrame instructionsFrame;
+
 
 	/**
 	 * 
@@ -22,44 +17,23 @@ public class JumpInView extends JFrame {
 	 * @param columns is the number of columns
 	 * @param rows is the number of rows
 	 */
-	public JumpInView(JumpInGame g) {
-		this.game = g;
-		
+	public JumpInView(JumpInGame game) {
 		//Setting up frame to display rules and challenges
-		preFrame = new InstructionsFrame(this);
+		instructionsFrame = new InstructionsFrame(this);
 		
 		//Setting up frame to display game
-		frame = new JFrame("Jump-In Game");
-    	gridPanel = new GridPanel(game);
-    	
-    	//setting arrow panel with all arrow buttons
-        arrowPanel = new ArrowPanel(game);
-        
-        
-        //creating console for output (game over)
-        textPanel = new ConsoleOutputPanel(game);
-	
-        //Creating menu bar
-        JMenuBar menuBar = new GameMenuBar(game, this);
-        	
-        
-        frame.setSize(700, 700);
-        frame.setLocationRelativeTo(null);
-        frame.setJMenuBar(menuBar);
-        frame.add(arrowPanel, BorderLayout.SOUTH);	
-    	frame.add(gridPanel, BorderLayout.CENTER);
-    	frame.add(textPanel, BorderLayout.NORTH);    	
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		gameFrame = new GameFrame(game, this);
+ 
 		
 	}
 	
 	public void toGameFrame() {			
-	    preFrame.setVisible(false);
-    	frame.setVisible(true);	    	
+	    instructionsFrame.setVisible(false);
+    	gameFrame.setVisible(true);	    	
     }
 	
 	public void toGameMenu() {			
-	    preFrame.setVisible(true);
-    	frame.setVisible(false);	    	
+	    instructionsFrame.setVisible(true);
+    	gameFrame.setVisible(false);	    	
     }
 }
