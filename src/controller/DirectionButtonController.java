c461737fe5d042208d660a3e9e6dd4fef9f0bfe5
noahmank <noah.rmank@gmail.com>
2019-11-17 02:51:14 -0500

    Implement DirectionButtonController

--- src/controller/DirectionButtonController.java
+++ src/controller/DirectionButtonController.java
@@ -3,12 +3,23 @@ package controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class DirectionButtonController implements ActionListener {
+import model.JumpInGame;
+import view.DirectionButton;
+import view.JumpInView;
 
+public class DirectionButtonController implements ActionListener {
+	private JumpInGame game;
+	private JumpInView view;
+	
+	public DirectionButtonController(JumpInGame game, JumpInView view) {
+		this.game = game;
+		this.view = view;
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
+		if(game.getPiece() != null) {
+			game.setDirection(((DirectionButton) e.getSource()).getDirection());
+		}
 	}
-
 }
