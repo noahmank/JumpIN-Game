71563c19f0411e2dbe9c7410ba3826d4f6fec338
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-18 17:17:45 -0500

    Added Javadoc

--- src/model/MoveAction.java
+++ src/model/MoveAction.java
@@ -1,36 +1,71 @@
 package model;
 
+/**
+* Class for an action of a moveable piece in a certain direction, involving getters and setters of the piece
+* as well as direction
+*/
 public class MoveAction {
 	private MoveablePiece piece;
 	private Direction direction;
 	
+	/**
+	* constructor to initialize piece and direction
+	* @param piece the moveable piece 
+	* @param direction the specified direction
+	*/
 	public MoveAction(MoveablePiece piece, Direction direction) {
 		this.piece = piece;
 		this.direction = direction;
 	}
 	
+	/**
+	* constructor to initialize piece and direction, it is 
+	* a constructor that is passed an instance of itself
+	* @param move includes the move action 
+	*/
 	public MoveAction(MoveAction move) {
 		this.piece = move.getPiece();
 		this.direction = move.getDirection();
 	}
 	
+	/**
+	* gets a new move action the opposite way of the specified direction
+	* with a moveable piece
+	* @return new instance of move action with opposite direction
+	*/
 	public MoveAction getOppositeMove() {
 		return new MoveAction(piece, direction.getOpposite());
 	}
 	
+	/**
+	* gets the direction
+	* @return direction
+	*/
 	public Direction getDirection() {
 		return this.direction;
 	}
 	
+	/**
+	* gets the piece
+	* @return piece
+	*/
 	public MoveablePiece getPiece() {
 		return this.piece;
 	}
 	
+	/**
+	* creates a string implementation of the move action
+	*/
 	@Override
 	public String toString() {
 		return "(" + this.piece.toString() + ", " + this.direction.toString() + ")";
 	}
 	
+	/**
+	* compares two objects to determine if they are equals
+	* @param obj is the object to be compared
+	* @return true or false if the objects are equals or not
+	*/
 	@Override
 	public boolean equals(Object obj) {
 		if(this == obj) return true;
