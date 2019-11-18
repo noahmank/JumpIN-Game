883b5abd8b6295632ef35c7f711203b95ff9fe1d
noahmank <noah.rmank@gmail.com>
2019-11-18 18:55:49 -0500

    Add Javadoc for ChallengeSelectController

--- src/controller/ChallengeSelectController.java
+++ src/controller/ChallengeSelectController.java
@@ -7,13 +7,25 @@ import javax.swing.JComboBox;
 
 import model.JumpInGame;
 
+/**
+ * Controller for the challenge select JComboBox
+ * @author Noah Mank
+ *
+ */
 public class ChallengeSelectController implements ActionListener {
 	private JumpInGame game;
 	
+	/**
+	 * Constructor for the ChallengeSelectController
+	 * @param game is the game with methods to be called when an action is performed
+	 */
 	public ChallengeSelectController(JumpInGame game) {
 		this.game = game;
 	}
 	
+	/**
+	 * Method that is called when a new item is selected in the JComboBox. Updates the challenge selected in model.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JComboBox<Integer> box = (JComboBox<Integer>) e.getSource();
