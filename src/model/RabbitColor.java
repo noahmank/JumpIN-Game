ca1d97d13413aece65922e4ad0c5aaddc70b9543
noahmank <noah.rmank@gmail.com>
2019-11-18 19:14:03 -0500

    Add Javadoc for RabbitColor

--- src/model/RabbitColor.java
+++ src/model/RabbitColor.java
@@ -1,5 +1,10 @@
 package model;
 
+/**
+ * Enumeration to identify rabbits by colour
+ * @author Noah Mank
+ *
+ */
 public enum RabbitColor {
 	GREY("greyRabbit.png"),
 	BROWN("brownRabbit.png"),
@@ -7,10 +12,18 @@ public enum RabbitColor {
 	
 	private final String src;
 	
+	/**
+	 * Assign the appropriate image for each colour rabbit
+	 * @param src is the name of the rabbit's image
+	 */
 	RabbitColor(String src) {
 		this.src = src;
 	}
 	
+	/**
+	 * Get the rabbit image for a given colour rabbit
+	 * @return the name of the rabbit's image
+	 */
 	public String getSrc() {
 		return this.src;
 	}
