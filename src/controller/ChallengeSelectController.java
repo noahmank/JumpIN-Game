889f83cb1ca918f95529558b2abdb68cb3eb815b
Liya <Owner@172.17.93.217>
2019-11-22 14:04:50 -0500

    Fixed subscription for ChallengeSelect view and controller

--- src/controller/ChallengeSelectController.java
+++ src/controller/ChallengeSelectController.java
@@ -6,25 +6,31 @@ import java.awt.event.ActionListener;
 import javax.swing.JComboBox;
 
 import model.JumpInGame;
+import view.ChallengeSelectPanel;
 
 /**
  * Controller for the challenge select JComboBox
+ * 
  * @author Noah Mank
  *
  */
 public class ChallengeSelectController implements ActionListener {
 	private JumpInGame game;
-	
+	private ChallengeSelectPanel view;
+
 	/**
 	 * Constructor for the ChallengeSelectController
+	 * 
 	 * @param game is the game with methods to be called when an action is performed
 	 */
-	public ChallengeSelectController(JumpInGame game) {
+	public ChallengeSelectController(JumpInGame game, ChallengeSelectPanel challengeSelectPanel) {
 		this.game = game;
+		this.view = challengeSelectPanel;
 	}
-	
+
 	/**
-	 * Method that is called when a new item is selected in the JComboBox. Updates the challenge selected in model.
+	 * Method that is called when a new item is selected in the JComboBox. Updates
+	 * the challenge selected in model.
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
--- src/view/ChallengeSelectPanel.java
+++ src/view/ChallengeSelectPanel.java
@@ -1,9 +1,7 @@
 package view;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,25 +15,29 @@ import model.JumpInGame;
  *
  */
 public class ChallengeSelectPanel extends JPanel {
-	
+
 	private JumpInGame game;
-	
+
 	/**
 	 * The challenge drop down panel
+	 * 
 	 * @param g is the JumpIn game model
 	 */
 	public ChallengeSelectPanel(JumpInGame g) {
 		super();
 		this.game = g;
-		Integer[] challenges = new Integer[game.getNumChallenges()]; 
-		// Initialize array with number (i + 1) at index i, up to number of challenges in game
-		for(int i = 0; i < game.getNumChallenges(); i++) {
+
+		Integer[] challenges = new Integer[game.getNumChallenges()];
+
+		// Initialize array with number (i + 1) at index i, up to number of challenges
+		// in game
+		for (int i = 0; i < game.getNumChallenges(); i++) {
 			challenges[i] = i + 1;
 		}
 		JComboBox<Integer> challengeList = new JComboBox<>(challenges);
 		challengeList.setSelectedIndex(0);
-		challengeList.addActionListener(new ChallengeSelectController(this.game));
-		
+		challengeList.addActionListener(new ChallengeSelectController(this.game, this));
+
 		JLabel selectChallengeText = new JLabel("Select a challenge:");
 		this.add(selectChallengeText, BorderLayout.CENTER);
 		this.add(challengeList, BorderLayout.SOUTH);
