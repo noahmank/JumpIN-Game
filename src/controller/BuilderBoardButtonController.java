06a273ed10ecd700c358d5266883c0dbd6d672e7
Noah <Noah@172.17.50.89>
2019-11-22 12:35:55 -0500

    Create new controller for board grid in builder

---
+++ src/controller/BuilderBoardButtonController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.*;
+import view.BoardButton;
+
+public class BuilderBoardButtonController implements ActionListener {
+
+	private JumpInGame game;
+	
+	/**
+	 * Instantiates the game board button controller
+	 * @param game is the JumpIn game model
+	 */
+	public BuilderBoardButtonController(JumpInGame game) {
+		this.game = game;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		BoardButton button = (BoardButton) e.getSource();
+		BoardBuilder builder = game.getBuilder();
+		int row = button.getRow();
+		int column = button.getColumn();
+		builder.setColumn(column);
+		builder.setRow(row);	
+	}
+}
