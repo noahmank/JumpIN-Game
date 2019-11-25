97e729d655ed82f5e391a6ab5058b583e3905a72
noahmank <noah.rmank@gmail.com>
2019-11-25 15:43:37 -0500

    Remove unnecessary methods

--- src/view/BoardButton.java
+++ src/view/BoardButton.java
@@ -11,7 +11,6 @@ import javax.swing.JButton;
 public class BoardButton extends JButton {
 	private int row;
 	private int column;
-	private Color color;
 	
 	/** Constructor of a BoardButton
 	 * @param column -> column number of button
@@ -21,7 +20,6 @@ public class BoardButton extends JButton {
 	public BoardButton(int column, int row, Color color) {
 		this.column = column;
 		this.row = row;
-		this.color = color;
 		this.setBackground(color);
 	}
 	
@@ -42,39 +40,4 @@ public class BoardButton extends JButton {
 	public int getColumn() {
 		return this.column;
 	}
-
-	/**
-	* Returns the color
-	*/
-	public Color getColor() {
-		return color;
-	}
-
-	/**
-	* Sets the color
-	*/
-	public void setColor(Color color) {
-		this.color = color;
-	}
-	
-	/**
-	* Overrides and implements the toString method
-	*/
-	@Override
-	public String toString() {
-		return "(" + this.getColumn() + ", " + this.getRow() + ", " + this.getColor() + ")";
-	}
-
-	/**
-	* Equals method to check if objects are equal to one another
-	* @returns a boolean value whether the objects are equal(true) or not (false)
-	*/
-	@Override 
-	public boolean equals(Object o) {
-		if(o == this) return true;
-		if(o == null || this.getClass() != o.getClass()) return false;
-		BoardButton b = (BoardButton) o;
-		return ((this.getRow() == b.getRow()) && (this.getColumn() == b.getColumn()) && (this.getColor() == b.getColor()));
-	}
-
 }
