6f431ee6e792f36b38c8595f187b22f723e1abdf
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-25 19:00:15 -0500

    Update JumpInGame to fix errors and add new tests

--- src/modeltests/JumpInGameTest.java
+++ src/modeltests/JumpInGameTest.java
@@ -51,7 +51,9 @@ public class JumpInGameTest {
 	@Test
 	public void testMovePiece() {
 		game.startChallenge();
-		game.movePiece(rabbit, Direction.SOUTH);
+		game.savePieceToMove(3, 0);
+		game.setDirection(Direction.SOUTH);
+		game.movePiece();
 		assertEquals("Expected grey rabbit at this location", "Grey Rabbit", board.getHole(3, 2).toString());
 	}
 	
@@ -63,7 +65,9 @@ public class JumpInGameTest {
 		game.addPieceToBoard(rabbit, 3, 0);
 		game.addPieceToBoard(mushroom, 3, 1);
 		game.addPieceToBoard(mushroom, 3, 2);
-		game.movePiece(rabbit, Direction.SOUTH);
+		game.savePieceToMove(3, 0);
+		game.setDirection(Direction.SOUTH);
+		game.movePiece();
 		assertEquals("Expected mushroom at this location", "MS", board.getHole(3, 2).toString());
 	}
 	
@@ -74,7 +78,9 @@ public class JumpInGameTest {
 	public void testMovePieceOutOfBounds() {
 		game.addPieceToBoard(rabbit, 3, 1);
 		game.addPieceToBoard(mushroom, 3, 0);
-		game.movePiece(rabbit, Direction.NORTH);
+		game.savePieceToMove(3, 1);
+		game.setDirection(Direction.NORTH);
+		game.movePiece();
 		assertEquals("Expecting Rabbit in the same spot", "Grey Rabbit", board.getHole(3, 1).toString());
 	}
 	
@@ -106,14 +112,52 @@ public class JumpInGameTest {
 	}
 	
 	/**
+	 * Tests challenge method
+	 * with challenge two
+	 */
+	@Test
+	public void testChallengeTwo() {
+		game.setSelectedChallenge(2);
+		game.startChallenge();
+		assertEquals("Expected Mushroom at (0, 1)", "MS", board.getHole(0, 1).toString());
+		assertEquals("Expected Mushroom at (0, 2)", "MS", board.getHole(0, 2).toString());
+		assertEquals("Expected Mushroom at (0, 4)", "MS", board.getHole(0, 4).toString());
+		assertEquals("Expected Grey Rabbit at (1, 0)", "Grey Rabbit", board.getHole(1, 0).toString());
+		assertEquals("Expected White Rabbit at (4, 1)", "White Rabbit", board.getHole(4, 1).toString());
+		assertEquals("Expected Brown Rabbit at (3, 0)", "Brown Rabbit", board.getHole(3, 0).toString());
+		assertEquals("Expected Fox 1 at (1, 2)", "Fox 1", board.getHole(1, 2).toString());
+		assertEquals("Expected Fox 2 at (2, 3)", "Fox 2", board.getHole(2, 3).toString());	
+	}
+	
+	/**
+	 * Tests challenge method
+	 * with challenge three
+	 */
+	@Test
+	public void testChallengeThree() {
+		game.setSelectedChallenge(3);
+		game.startChallenge();
+		assertEquals("Expected Mushroom at (3, 0)", "MS", board.getHole(3, 0).toString());
+		assertEquals("Expected Mushroom at (2, 2)", "MS", board.getHole(2, 2).toString());
+		assertEquals("Expected Mushroom at (4, 4)", "MS", board.getHole(4, 4).toString());
+		assertEquals("Expected Grey Rabbit at (0, 3)", "Grey Rabbit", board.getHole(0, 3).toString());
+		assertEquals("Expected White Rabbit at (3, 1)", "White Rabbit", board.getHole(3, 1).toString());
+		assertEquals("Expected Brown Rabbit at (4, 2)", "Brown Rabbit", board.getHole(4, 2).toString());
+		assertEquals("Expected Fox 1 at (0, 1)", "Fox 1", board.getHole(0, 1).toString());
+		assertEquals("Expected Fox 2 at (3, 3)", "Fox 2", board.getHole(3, 3).toString());
+	}
+	
+	
+	/**
 	 * Tests undo method
 	 */
 	@Test
 	public void testUndo() {
 		game.startChallenge();
-		game.movePiece(rabbit, Direction.SOUTH);
-		System.out.println(game.getBoard().toString());
-		game.movePiece(rabbit, Direction.SOUTH);
+		game.savePieceToMove(3, 0);
+		game.setDirection(Direction.SOUTH);
+		game.movePiece();
+		game.movePiece();
 		game.undoMoveAction();
 		assertEquals("Expecting Grey Rabbit at (3, 2)", "Grey Rabbit", game.getBoard().getPieceFromGrid(3, 2).toString());
 	}
@@ -124,8 +168,10 @@ public class JumpInGameTest {
 	@Test
 	public void testRedo() {
 		game.startChallenge();
-		game.movePiece(rabbit, Direction.SOUTH);
-		game.movePiece(rabbit, Direction.SOUTH);
+		game.savePieceToMove(3, 0);
+		game.setDirection(Direction.SOUTH);
+		game.movePiece();
+		game.movePiece();
 		game.undoMoveAction();
 		game.undoMoveAction();
 		game.redoMoveAction();
@@ -133,6 +179,15 @@ public class JumpInGameTest {
 		game.redoMoveAction();
 		assertEquals("Expecting Grey Rabbit at (3, 4)", "Grey Rabbit", board.getHole(3, 4).toString());
 	}
+	
+	/**
+	 * Test get Board
+	 */
+	@Test
+	public void testGetBoard() {
+		assertEquals("Expecting the board to be the same", board, game.getBoard());
+	}
+	
 	
 	/**
 	 * Test setPiece method
@@ -151,6 +206,16 @@ public class JumpInGameTest {
 		game.addPieceToBoard(rabbit, 3, 2);
 		assertEquals("Expecting Grey Rabbit", "Grey Rabbit", board.getHole(3,2).getPiece().toString());
 	}
+	
+	/**
+	 * Test set direction
+	 */
+	@Test
+	public void testSetDirection() {
+		game.setDirection(Direction.NORTH);
+		assertEquals("Expecting NORTH", Direction.NORTH, game.getDirection());
+	}
+	
 	/**
 	 * Test getDirection method
 	 */
@@ -158,6 +223,74 @@ public class JumpInGameTest {
 	public void testGetDirection() {
 		fox = new Fox(Direction.NORTH, 1);
 		assertEquals("Expecting NORTH", Direction.NORTH, fox.getDirection());
+	}
+		
+	/**
+	 * test set console output
+	 */
+	@Test
+	public void testSetAndGetConsoleOutput() {
+		game.setConsoleOutput("Rabbit moved South");
+		assertEquals("Expecting 'Rabbit moved South'", "Rabbit moved South", game.getConsoleOutput());
+	}
+	
+	
+	
+	/**
+	 * test next move from solution
+	 * and solve game method
+	 */
+	@Test
+	public void testDoNextMoveFromSolutionChallengeOne() {
+		
+	}
+	
+	
+	/**
+	 * test get selected challenge
+	 */
+	@Test
+	public void testSetAndGetSelectedChallenge() {
+		game.setSelectedChallenge(1);
+		assertEquals("Expecting 1", 1, game.getSelectedChallenge());
+		game.setSelectedChallenge(2);
+		assertEquals("Expecting 2", 2, game.getSelectedChallenge());
+		game.setSelectedChallenge(3);
+		assertEquals("Expecting 3", 3, game.getSelectedChallenge());
+	}
+	
+	/**
+	 * test get number challenges
+	 */
+	@Test
+	public void testGetNumChallenges() {
+		assertEquals("Expecting 3 so far", 3, game.getNumChallenges());
+	}
+	
+	/**
+	 * test save piece to move
+	 */
+	@Test
+	public void testSavePieceToMove() {
+		game.startChallenge();
+		game.savePieceToMove(3, 0);
+		assertEquals("Expecting Grey Rabbit", "Grey Rabbit", game.getPiece().toString());
+	}
+	
+	/**
+	 * test start builder
+	 */
+	@Test
+	public void testStartBuilder() {
+		
+	}
+	
+	/**
+	 * test start builder
+	 */
+	@Test
+	public void testGetBuilder() {
+		
 	}
 
 }
