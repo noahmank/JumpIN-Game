9d1a619a84404d300060900c7e7c0125aa88f271
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-28 11:47:28 -0500

    Create BoardBuilderTest.java

---
+++ src/modeltests/BoardBuilderTest.java
@@ -0,0 +1,80 @@
+package modeltests;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import model.*;
+
+/**
+ * 
+ * @author adelatullio
+ *
+ */
+public class BoardBuilderTest {
+	private BoardBuilder boardBuilder;
+	private GameBoard board;
+	private Mushroom mushroom;
+	
+	/**
+	 * sets up variables for testing
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		board = new GameBoard();
+		boardBuilder = new BoardBuilder(board);
+		mushroom = new Mushroom();
+	}
+
+	/**
+	 * tears down variables after testing
+	 * @throws Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		board = null;
+		boardBuilder = null;
+		mushroom = null;
+	}
+
+	/**
+	 * tests the setter and getter for column
+	 */
+	@Test
+	public void testSetAndGetColumn() {
+		boardBuilder.setColumn(5);
+		assertEquals("Expecting 5", 5, boardBuilder.getColumn());
+	}
+	
+	/**
+	 * tests the setter and getter for row
+	 */
+	@Test
+	public void testSetAndGetRow() {
+		boardBuilder.setRow(5);
+		assertEquals("Expecting 5", 5, boardBuilder.getRow());
+	}
+	
+	/**
+	 * tests the setter and getter for selected piece
+	 */
+	@Test
+	public void testSetAndGetSelectedPiece() {
+		boardBuilder.setSelectedPiece(mushroom);
+		assertEquals("Expecting mushroom", mushroom, boardBuilder.getSelectedPiece());
+	}
+	
+	/**
+	 * tests add piece to board method
+	 */
+	@Test
+	public void testAddPieceToBoard() {
+		boardBuilder.setColumn(2);
+		boardBuilder.setRow(1);
+		boardBuilder.setSelectedPiece(mushroom);
+		boardBuilder.addPieceToBoard();
+		board = boardBuilder.getBoardToBuild();
+		assertEquals("Expecting mushroom at Column 2, Row 1", "MS", board.getHole(2, 1).toString());
+	}
+}
