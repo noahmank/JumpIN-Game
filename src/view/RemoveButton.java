f752b96ddb78aa5f1f01f1a72530eaea236229f3
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-11-30 10:41:10 -0500

    Update RemoveButton view

--- src/view/RemoveButton.java
+++ src/view/RemoveButton.java
@@ -2,10 +2,11 @@ package view;
 
 import java.awt.event.ActionListener;
 
+
 import javax.swing.JButton;
 
 import controller.RemoveButtonController;
-import model.JumpInGame;
+import model.*;
 /**
  * The Remove Button view
  * @author Adela Tullio
@@ -16,7 +17,7 @@ public class RemoveButton extends JButton{
 	private RemoveButtonController removeButtonController;
 	private int column;
 	private int row;
-	private JumpInGame game;
+	private BoardBuilder build;
 
 	/**
 	 * remove button constructor
@@ -24,14 +25,14 @@ public class RemoveButton extends JButton{
 	 * @param column is the column of the piece you wish to remove
 	 * @param row is the row of the piece you wish to remove
 	 */
-	public RemoveButton(JumpInGame g, int column, int row) {
+	public RemoveButton(BoardBuilder build, int column, int row) {
 		super();
-		this.game = g;
+		this.build = build;
 		this.column = column;
 		this.row = row;
 		removeButton = new JButton("Remove Piece");
 		this.add(removeButton);
-		removeButtonController = new RemoveButtonController(this.game, this);
+		removeButtonController = new RemoveButtonController(this.build, this);
 	}
 
 	/**
