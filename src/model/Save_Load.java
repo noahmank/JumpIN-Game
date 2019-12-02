6657e342eb7e1aa2fc9615a28a3a822f6291a985
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-12-02 13:20:13 -0500

    Add Javadoc

--- src/model/Save_Load.java
+++ src/model/Save_Load.java
@@ -6,9 +6,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-
+/**
+* Save and Load class Implementation
+*/
 public class Save_Load {
-
+	
+	/**
+	* A method that saves the Game state to a file
+	* @param game is the game to be saved
+	* @param Filename is the name of the file the game will be saved to
+	* @throws IOException 
+	*/
 	public static void saveGameDataToFile(JumpInGame game, String Filename) throws IOException {
 
 		FileOutputStream fileStream = new FileOutputStream(Filename);
@@ -20,7 +28,13 @@ public class Save_Load {
 		fileStream.close();
 
 	}
-
+	
+	/**
+	* A method that loads the Game state
+	* @param filename is the filename to load data from
+	* @return Object of the loaded gane data
+	* @throws ClassNotFoundException, IOException
+	*/
 	public static Object loadGameDataFromFile(String filename) throws ClassNotFoundException, IOException {
 
 		File file = new File(filename);
