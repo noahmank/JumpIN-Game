2341bf6e1f36cec12727651331715bf2a5778715
Aubin <Aubin@DESKTOP-AJADR1N>
2019-12-02 11:46:31 -0500

    WIP: Still tweeking alot

--- src/model/LevelBuilderXMLParser.java
+++ src/model/LevelBuilderXMLParser.java
@@ -18,33 +18,39 @@ import jdk.internal.org.xml.sax.helpers.DefaultHandler;
  */
 public class LevelBuilderXMLParser extends DefaultHandler{
 	
-	GameBoard level = null;
-	Fox fox;
-	Rabbit rabbit;
-	Mushroom mushroom;
-	int number = 0;
-	Direction direction = null;
-	Point location = null;
-	RabbitColor color = null;
+	private GameBoard level = null;
+	private Fox fox;
+	private Rabbit rabbit;
+	private Mushroom mushroom;
+	private String number;
+	private String direction;
+	private String location;
+	private String color;
 	
 		
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		level = new GameBoard();
-		number = -1;
-		direction = null;
-		location = null;
-		color = null;
+		number = "";
+		direction = "";
+		location = "";
+		color = "";
 	}
 		
-	public void endElement(String uri, String localName, String qName) {
+	public void endElement(String uri, String localName, String qName)  {
 		switch(qName) {
-			case "Fox": fox = new Fox(direction,number);
-						level.addPiece(fox, location.x, location.y);
+			case "Fox": Direction dr = (direction == "EAST")? Direction.EAST : (direction == "WEST")? Direction.WEST : 
+						(direction == "NORTH")? Direction.NORTH : (direction == "SOUTH")? Direction.SOUTH : null;
+						//fox = new Fox(dr,Integer.parseInt(number));
+						//level.addPiece(fox, location.x, location.y);
 				case "Rabbit": rabbit = new Rabbit(color);
 						level.addPiece(rabbit, location.x, location.y);
 				case "Mushroom" : mushroom = new Mushroom();
 						level.addPiece(mushroom, location.x, location.y);
 			}
 		}
+	
+	public void characters(char[] ch, int start, int length) {
+		
+	}
 
 }
