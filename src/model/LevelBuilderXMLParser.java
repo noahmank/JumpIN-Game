54eb081fc16762f2c4caceb568c918333e38092b
Aubin <Aubin@DESKTOP-AJADR1N>
2019-12-02 14:29:39 -0500

    Added logic to type conversions

--- src/model/LevelBuilderXMLParser.java
+++ src/model/LevelBuilderXMLParser.java
@@ -3,10 +3,6 @@
  */
 package model;
 
-import java.awt.Point;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -26,7 +22,9 @@ public class LevelBuilderXMLParser extends DefaultHandler{
 	private String location;
 	private String color;
 	
-		
+	/**	
+	 * 
+	 */
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
 		level = new GameBoard();
 		number = "";
@@ -34,21 +32,35 @@ public class LevelBuilderXMLParser extends DefaultHandler{
 		location = "";
 		color = "";
 	}
-		
+	
+	/**
+	 * 
+	 */
 	public void endElement(String uri, String localName, String qName)  {
 		switch(qName) {
+		
 			case "Fox": Direction dr = (direction == "EAST")? Direction.EAST : (direction == "WEST")? Direction.WEST : 
-						(direction == "NORTH")? Direction.NORTH : (direction == "SOUTH")? Direction.SOUTH : null;
-						//fox = new Fox(dr,Integer.parseInt(number));
-						//level.addPiece(fox, location.x, location.y);
-				case "Rabbit": rabbit = new Rabbit(color);
-						level.addPiece(rabbit, location.x, location.y);
-				case "Mushroom" : mushroom = new Mushroom();
-						level.addPiece(mushroom, location.x, location.y);
+				(direction == "NORTH")? Direction.NORTH : (direction == "SOUTH")? Direction.SOUTH : null;
+				fox = new Fox(dr,Integer.parseInt(number));
+				//level.addPiece(fox, location.x, location.y);
+			case "Rabbit": RabbitColor rabbitColor = (color == "WHITE")? RabbitColor.WHITE : (color == "BROWN")? RabbitColor.BROWN :
+				(color == "GREY")? RabbitColor.GREY: null;
+				rabbit = new Rabbit(rabbitColor);
+				//level.addPiece(rabbit, location.x, location.y);
+			case "Mushroom" : mushroom = new Mushroom();
+				//level.addPiece(mushroom, location.x, location.y);
 			}
 		}
 	
+	/**
+	 * 
+	 */
 	public void characters(char[] ch, int start, int length) {
+		
+		number = new String(ch, start, length);
+		direction = new String(ch, start, length);
+		location = new String(ch, start, length);
+		color = new String(ch, start, length);
 		
 	}
 
