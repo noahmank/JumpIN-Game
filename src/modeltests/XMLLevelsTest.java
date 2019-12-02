1b5fae5fedb317ee20a0c832e5caf83c82c58343
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-12-02 14:35:57 -0500

    Create XMLLevelsTest.java (unimplemented)

---
+++ src/modeltests/XMLLevelsTest.java
@@ -0,0 +1,57 @@
+package modeltests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+/**
+ * tests the XMLLevels class
+ * @author adelatullio
+ *
+ */
+public class XMLLevelsTest {
+	
+	/**
+	 * sets up the private variables
+	 * for the tests
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	/**
+	 * tears down the private variables
+	 * after the tests
+	 * @throws Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	/**
+	 * tests getting the board to XML format
+	 */
+	@Test
+	public void testBoardToXML() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * tests level one to XML Format
+	 */
+	@Test
+	public void testExportBoardLevelOneToXML() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * tests importing from XML
+	 */
+	@Test
+	public void testImportFromXML() {
+		fail("Not yet implemented");
+	}
+
+}
