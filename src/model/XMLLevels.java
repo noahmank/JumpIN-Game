7596d54fa4ebcb17bdb1d3a49c98e5117e3567b9
Aubin <Aubin@DESKTOP-AJADR1N>
2019-12-01 21:19:39 -0500

    WIP: XML level builder

--- src/model/XMLLevels.java
+++ src/model/XMLLevels.java
@@ -3,10 +3,8 @@
  */
 package model;
 
-import java.io.FileReader; 
-import java.util.Iterator; 
-import java.util.Map; 
-  
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * @author Aubin
@@ -14,13 +12,71 @@ import java.util.Map;
  */
 public class XMLLevels {
 	
+	private GameBoard board;
 	
 	
 	/**
-	 * Constructing a file containing the levels
+	 * Constructing a file containing a level
+	 * @param fileName is the name of the file
 	 */
-	public XMLLevels() {
+	public XMLLevels(String fileName, GameBoard board) {
+		try {
+			FileWriter fileWriter = new FileWriter(fileName);
+		}
+		catch(IOException exc){
+			System.out.println("Filename not available");
+		}
+		this.board = board;
 		
 	}
+	
+	/**
+	 * Presents the Gameboard in XML format
+	 * @return
+	 */
+	public String boardToXML() {
+		
+		String boardXML = "<GameBoard>\n";
+		
+		//Looping through the given board
+		for(int i = 0; i <= this.board.getNumColumns(); i++) {
+			for(int j = 0; j <= this.board.getNumRows(); j++) {
+				
+				if(this.board.getPieceFromGrid(i, j) instanceof Fox) {
+					Fox fox = (Fox) this.board.getPieceFromGrid(i, j);
+					boardXML += "	<Fox>\n";
+					boardXML += "		<Number>" + fox.getNumber() + "</Number>";
+					boardXML += "		<Direction>" + fox.getDirection() + "</Direction>";
+					boardXML += "		<Location>" + i + j + "</Location>";//might need to change this format
+					boardXML += "	</Fox>\n";
+				}
+				else if(this.board.getPieceFromGrid(i, j) instanceof Rabbit) {
+					Rabbit rabbit = (Rabbit) this.board.getPieceFromGrid(i, j);
+					boardXML += "	<Rabbit>\n";
+					boardXML += "		<Color>" + rabbit.getColor() + "</Color>";
+					boardXML += "		<Location>" + i + j + "</Location>";//might need to change this format
+					boardXML += "	</Rabbit>\n";
+				}
+				else if(this.board.getPieceFromGrid(i, j) instanceof Mushroom) {
+					boardXML += "	<Mushroom>\n";
+					boardXML += "		<Location>" + i + j + "</Location>";//might need to change this format
+					boardXML += "	</Mushroom>\n";
+				}
+			}
+		}
+		
+		boardXML += "</GameBoard>\n";
+		
+		return boardXML;
+	}
+	
+	/**
+	 * Adds the level to in XML format into the file
+	 */
+	public void levelToXML() {
+		
+	}
+	
+	
 
 }
