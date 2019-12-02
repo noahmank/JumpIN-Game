be8dd5f35fc37e5774ea948653dc5aefc32b2eb7
adelatullio <55845588+adelatullio@users.noreply.github.com>
2019-12-02 14:36:37 -0500

    Create LevelBuilderXMLParserTest.java (unimplemented)

---
+++ src/modeltests/LevelBuilderXMLParserTest.java
@@ -0,0 +1,50 @@
+package modeltests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+/**
+ * tests the LevelBuilderXMLParser class
+ * @author adelatullio
+ *
+ */
+public class LevelBuilderXMLParserTest {
+	
+	/**
+	 * sets up the private variables
+	 * for the tests
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+
+	/**
+	 * tears down the private variables
+	 * after the tests
+	 * @throws Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	/**
+	 * tests start element method
+	 */
+	@Test
+	public void testStartElement() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * tests end element method
+	 */
+	@Test
+	public void testEndElement() {
+		fail("Not yet implemented");
+	}
+
+}
