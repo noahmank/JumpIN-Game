aaf005487002a64a3ebccc8bf6d90477c13bdb23
Aubin <Aubin@DESKTOP-AJADR1N>
2019-12-01 20:20:22 -0500

    Looking into implemeneting the JSONParse class

---
+++ src/model/JSONLevels.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package model;
+
+import java.io.FileReader; 
+import java.util.Iterator; 
+import java.util.Map; 
+  
+
+/**
+ * @author Aubin
+ *
+ */
+public class JSONLevels {
+	
+	
+	
+	/**
+	 * Constructing a file containing the levels
+	 */
+	public JSONLevels() {
+		
+	}
+
+}
